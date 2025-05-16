8fc59e07f782c7be0424525f0c147058db417712
DanielKGM <atbreezy4@gmail.com>
2025-05-16 17:07:39 -0300

    binaryoperator

---
+++ StreamAPI/src/exemplos/BinaryOperatorExemplo.java
@@ -0,0 +1,15 @@
+package exemplos;
+
+/*
+ * FUNCTIONS -> argumento e resultado -> usa no map()
+ * CONSUMER -> argumento sem resultado -> usa no forEach()
+ * SUPPLIER -> resultado sem argumento -> usa no generate()
+ * 
+ * PEDICATE -> argumento e resultado em BOOLEAN de acordo com uma condição -> usa no filter()
+ * BINARYOPERATOR -> dois argumentos e resultado -> usa no reduction() 
+ */
+public class BinaryOperatorExemplo {
+    public static void main(String[] args) {
+        //
+    }
+}
